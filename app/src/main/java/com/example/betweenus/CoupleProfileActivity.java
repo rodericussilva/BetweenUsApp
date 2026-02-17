71484abcd5fb0a5af051e5db06535ef5b1edead5
Rodrigo Silva <rodericus@ufc.alu.br>
2026-02-17 10:28:22 -0300

    construction of the couple profile screen

---
+++ app/src/main/java/com/example/betweenus/CoupleProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.betweenus;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CoupleProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_couple_profile);
+
+        // Depois vamos carregar dados do casal aqui
+    }
+}
